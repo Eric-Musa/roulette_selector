cd907eefc68e2fe838b28fe4270f6de86c600852
ericmusa <ericmusa@umich.edu>
2021-08-20 14:40:57 -0400

    cleaned up setup.py

--- setup.py
+++ setup.py
@@ -1,16 +1,11 @@
 from setuptools import setup
 
-# with open("README.md", "r") as fh:
-#     long_description = fh.read()
-
 setup(
     name="roulette_selector",
     version="0.0.1",
     author="Eric Musa",
     author_email="eric.musa17@gmail.com",
     description="A small roulette-style random selector of weighted options",
-    # long_description=long_description,
-    # long_description_content_type="text/markdown",
     url="https://bitbucket.org/Eric-Musa/roulette/",
     py_modules=['roulette_selector'],
     package_dir={'': 'src'},
